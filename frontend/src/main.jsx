6f6da7f04dbb2f12fb64d9deab30c4e4cfa36081
Ben <code.hasan@protonmail.com>
2024-11-30 18:34:16 +0600

    color setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
+import { Router, RouterProvider } from 'react-router-dom';
 
 import './index.css';
 import Routers from './Routes.jsx'
